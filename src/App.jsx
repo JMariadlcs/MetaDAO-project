5420a7f9faad87eb3606e9b347222fc2032be00d
Chema De la Cruz Sanchez <chemadelacruzsanchez@MacBook-Pro-de-Chema-2.local>
2021-12-20 16:42:29 +0100

    governance token

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState} from "react";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 //import thirdweb provider
 import { useWeb3 } from "@3rdweb/hooks";
+import { ethers } from "ethers";
 
 // We instatiate the sdk on Rinkeby.
 const sdk = new ThirdwebSDK("rinkeby");
@@ -9,6 +10,10 @@ const sdk = new ThirdwebSDK("rinkeby");
 // We can grab a reference to our ERC-1155 contract.
 const bundleDropModule = sdk.getBundleDropModule(
   "0x7E4e74db6F5356aF7C3D6f8a7e6410C0d5d1056F",
+);
+
+const tokenModule = sdk.getTokenModule(
+  "0xE0dB1E14f0aAF18F829317259F08f81614433851"
 );
 
 const App = () => {
@@ -24,6 +29,68 @@ const App = () => {
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   // isClaiming lets us easily keep a loading state while the NFT is minting.
   const [isClaiming, setIsClaiming] = useState(false);
+
+          // Holds the amount of token each member has in state.
+const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+// The array holding all of our members addresses.
+const [memberAddresses, setMemberAddresses] = useState([]);
+
+// A fancy function to shorten someones wallet address, no need to show the whole thing. 
+const shortenAddress = (str) => {
+  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+};
+
+// This useEffect grabs all our the addresses of our members holding our NFT.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+  
+  // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+  // with tokenId 0.
+  bundleDropModule
+    .getAllClaimerAddresses("0")
+    .then((addresess) => {
+      console.log("🚀 Members addresses", addresess)
+      setMemberAddresses(addresess);
+    })
+    .catch((err) => {
+      console.error("failed to get member list", err);
+    });
+}, [hasClaimedNFT]);
+
+// This useEffect grabs the # of token each member holds.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+
+  // Grab all the balances.
+  tokenModule
+    .getAllHolderBalances()
+    .then((amounts) => {
+      console.log("👜 Amounts", amounts)
+      setMemberTokenAmounts(amounts);
+    })
+    .catch((err) => {
+      console.error("failed to get token amounts", err);
+    });
+}, [hasClaimedNFT]);
+
+// Now, we combine the memberAddresses and memberTokenAmounts into a single array
+const memberList = useMemo(() => {
+  return memberAddresses.map((address) => {
+    return {
+      address,
+      tokenAmount: ethers.utils.formatUnits(
+        // If the address isn't in memberTokenAmounts, it means they don't
+        // hold any of our token.
+        memberTokenAmounts[address] || 0,
+        18,
+      ),
+    };
+  });
+}, [memberAddresses, memberTokenAmounts]);
 
   // Another useEffect!
   useEffect(() => {
@@ -70,14 +137,39 @@ const App = () => {
   }
 
   //Case user already owns the DAO Acess NFT
-  if (hasClaimedNFT) {
-    return (
-      <div className="member-page">
-        <h1>🍪DAO Member Page</h1>
-        <p>You are already a member of METADAO!</p>
+  // If the user has already claimed their NFT we want to display the interal DAO page to them
+// only DAO members will see this. Render all the members + token amounts.
+if (hasClaimedNFT) {
+  return (
+    <div className="member-page">
+      <h1>🍪DAO Member Page</h1>
+      <p>Congratulations on being a member</p>
+      <div>
+        <div>
+          <h2>Member List</h2>
+          <table className="card">
+            <thead>
+              <tr>
+                <th>Address</th>
+                <th>Token Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {memberList.map((member) => {
+                return (
+                  <tr key={member.address}>
+                    <td>{shortenAddress(member.address)}</td>
+                    <td>{member.tokenAmount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
   const mintNft = () => {
     setIsClaiming(true);
