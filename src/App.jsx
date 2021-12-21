69f666b7760ece31c438c4c00b67f11d0a941cb4
Chema De la Cruz Sanchez <chemadelacruzsanchez@MacBook-Pro-de-Chema-2.local>
2021-12-21 14:06:29 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -207,7 +207,7 @@ const App = () => {
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1>🍪DAO Member Page</h1>
+        <h1>🌟 METADAO MEMBER PAGE 🌟</h1>
         <p>Congratulations on being a member</p>
         <div>
           <div>
@@ -384,7 +384,7 @@ const App = () => {
   // Render mint nft screen.
   return (
     <div className="mint-nft">
-      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <h1>Mint your free 🌟DAO Membership NFT</h1>
       <button
         disabled={isClaiming}
         onClick={() => mintNft()}
